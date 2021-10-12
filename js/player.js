bbdd56d71522de7be761d03d355240c16c12e5f9
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-10-12 11:26:04 -0500

    Volume handling and slider completed

--- js/player.js
+++ js/player.js
@@ -1,9 +1,11 @@
 const $playBtn = document.getElementById('play-button');
 const $audioPlayer = document.getElementById('audio');
-// const $progressBar = document.getElementById('progressBar');
+const $audioLimit = document.getElementById('audio-limit');
+const $audioCurrentTime = document.getElementById('audio-currentTime');
 
 $playBtn.addEventListener('click', handleAudio);
 $audioPlayer.addEventListener('loadedmetadata', setDuration);
+$audioPlayer.addEventListener('loadedmetadata', printDuration);
 $audioPlayer.addEventListener('timeupdate', handleTimeUpdate);
 $progressBar.addEventListener('input', handleProgressBar);
 
@@ -23,11 +25,30 @@ function setDuration() {
 	progressBar.max = $audioPlayer.duration;
 }
 
+function printDuration() {
+	const minutes = parseInt($audioPlayer.duration / 60);
+	const seconds = parseInt($audioPlayer.duration % 60).toLocaleString('en-US', {
+		minimumIntegerDigits: 2,
+		useGrouping: false,
+	});
+	$audioLimit.innerText = `${minutes}:${seconds}`;
+}
+
+function printCurrentTime() {
+	const minutes = parseInt($audioPlayer.currentTime / 60);
+	const seconds = parseInt($audioPlayer.currentTime % 60).toLocaleString(
+		'en-US',
+		{ minimumIntegerDigits: 2, useGrouping: false }
+	);
+	$audioCurrentTime.innerText = `${minutes}:${seconds}`;
+}
+
 function handleTimeUpdate() {
 	$progressBar.value = $audioPlayer.currentTime;
 	const progressValue = ($progressBar.value / $progressBar.max) * 100;
 
 	setCssVar('--progress', `${progressValue}%`);
+	printCurrentTime();
 }
 
 function handleProgressBar() {
--- js/volume.js
+++ js/volume.js
@@ -1,7 +1,30 @@
-// const $volume = document.getElementById('volume');
+const $volumeBtn = document.getElementById('volume-button');
+let currentVolume;
 
 $volume.addEventListener('input', setVolume);
+$volumeBtn.addEventListener('click', handleVolume);
 
 function setVolume() {
 	$audioPlayer.volume = $volume.value / 100;
+
+	if ($audioPlayer.volume === 0) {
+		$volumeBtn.className = 'player-control is-mute';
+	} else if ($audioPlayer.volume <= 0.25) {
+		$volumeBtn.className = 'player-control is-low';
+	} else if ($audioPlayer.volume <= 0.5) {
+		$volumeBtn.className = 'player-control is-normal';
+	} else {
+		$volumeBtn.className = 'player-control';
+	}
+}
+
+function handleVolume() {
+	const isMute = this.classList.toggle('is-mute');
+
+	if (isMute) {
+		currentVolume = $audioPlayer.volume;
+		$audioPlayer.volume = 0;
+	} else {
+		$audioPlayer.volume = currentVolume;
+	}
 }
